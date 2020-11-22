5508ce68063ec4d0e2dadab39fd62fe13148346c
ROHIT RANJAN <rohit.ranjan10@hotmail.com>
2020-11-22 19:58:26 +0530

    Driver initialization part

--- src/main/java/com/Pages/ContactsPage.java
+++ src/main/java/com/Pages/ContactsPage.java
@@ -1,9 +1,13 @@
 package com.Pages;
 
+import org.openqa.selenium.WebDriver;
+
 import com.base.BasePage;
 
 public class ContactsPage extends BasePage{
-	public ContactsPage() {
+	WebDriver driver;
+	public ContactsPage(WebDriver drive) {
+		this.driver = driver;
 		
 	}
 
